02f17e3961d444cc34d5b1b3f94516b47b5f3671
Bouwe K. Westerdijk <bouwe@bouwe.nl>
2019-05-11 21:13:34 +0200

    Initial commit

---
+++ components/MyConfetti.js
@@ -0,0 +1,12 @@
+import Confetti from 'react-confetti'
+import {useWindowSize} from 'react-use';
+
+const MyConfetti = () => {
+  const {width, height} = useWindowSize();
+
+  return (
+    <Confetti width={width} height={height} />
+  );
+};
+
+export default MyConfetti
---
+++ theme.js
@@ -0,0 +1,10 @@
+import { future as theme } from 'mdx-deck/themes'
+
+export default {
+  ...theme,
+  // custom colors
+  colors: {
+    text: '#ffffff',
+    background: '#262626',
+  },
+}
